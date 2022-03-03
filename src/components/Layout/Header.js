42e2f2d5cdd90b900bd1327f181e5d42e725fc4a
Matija <siouxx0612@gmail.com>
2022-03-03 11:03:10 +0100

    Install npm, Folders

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+
+const Header = (props) => {
+
+    return <Fragment>
+            <header></header>
+            <h1>Meals</h1>
+            <button></button>
+            <div></div>
+        </Fragment> 
+};
+
+export default Header;
