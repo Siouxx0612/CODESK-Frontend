5f091f6fd4541ac739c873b6d591ceca807ff7a7
Matija <siouxx0612@gmail.com>
2022-03-10 15:38:44 +0100

    Form add CSS, ransform, color, etc v.01

---
+++ src/components/Menu/MealItemForm.js
@@ -0,0 +1,11 @@
+
+import classes from '../Menu/MealItemForm.module.css';
+
+const MeaItemForm = props => {
+    return <form className={classes.form}>
+        <input />
+        <button>+ Add</button>
+    </form>
+};
+
+export default MeaItemForm;
--- src/components/Menu/Mealitem/MealItem.js
+++ src/components/Menu/Mealitem/MealItem.js
@@ -1,4 +1,5 @@
 import classes from '../Mealitem/MealItem.module.css';
+import MealItemForm from '../MealItemForm';
 
 const MealItem = (props) => {
 
@@ -6,12 +7,12 @@ const MealItem = (props) => {
     return (
         <li className={classes.meal}>
             <div>
-            <h3>{props.name}</h3>
-            <div className={classes.description}>{props.description}</div>
-            <div className={classes.price}>{price}</div>
+                <h3>{props.name}</h3>
+                <div className={classes.description}>{props.description}</div>
+                <div className={classes.price}>{price}</div>
             </div>
             <div>
-
+                <MealItemForm />
             </div>
         </li>
     );
