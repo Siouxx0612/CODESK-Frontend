8cdf13017c7f4ac8a5e447d5d8e333b0a5190b8f
Matija <siouxx0612@gmail.com>
2022-03-11 11:39:13 +0100

    add Form Input and label

--- src/components/Menu/MealItemForm.js
+++ src/components/Menu/MealItemForm.js
@@ -1,9 +1,16 @@
-
+import Input from '../UI/Input';
 import classes from '../Menu/MealItemForm.module.css';
 
 const MeaItemForm = props => {
     return <form className={classes.form}>
-        <input />
+        <Input label="Amount" input={{
+            id: "Amount",
+            type: "number",
+            min: 1,
+            max: 5,
+            step: 1,
+            initialvalue: 1,
+        }} />
         <button>+ Add</button>
     </form>
 };
