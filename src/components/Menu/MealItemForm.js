21ce3e72a9816a402f3499888c453b950b2ac6ad
Matija <siouxx0612@gmail.com>
2022-03-14 10:37:28 +0100

    fix For id

--- src/components/Menu/MealItemForm.js
+++ src/components/Menu/MealItemForm.js
@@ -3,14 +3,15 @@ import classes from '../Menu/MealItemForm.module.css';
 
 const MeaItemForm = props => {
     return <form className={classes.form}>
-        <Input label="Amount" input={{
-            id: "Amount",
-            type: "number",
-            min: 1,
-            max: 5,
-            step: 1,
-            initialvalue: 1,
-        }} />
+        <Input label="Amount"
+            input={{
+                id: "Amount" + props.id,
+                type: "number",
+                min: 1,
+                max: 5,
+                step: 1,
+                defaultValue: 1,
+            }} />
         <button>+ Add</button>
     </form>
 };
