254a267ddd8eb16da6725cb77221b2defec53fdb
Matija <siouxx0612@gmail.com>
2022-04-20 11:17:45 +0200

    Switch buttons on Delivery option

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,12 +2,20 @@ import { Fragment } from 'react';
 import mealsImg from '../../assets/wp3376124.jpeg';
 import classes from '../Layout/Header.module.css';
 import HeaderCartButton from '../Layout/HeaderCartButton';
+import { useState } from 'react';
 
-const reload = () =>  {
+const reload = () => {
     window.location.reload()
 };
 
 const Header = props => {
+
+    const [isActive, setIsActive] = useState(false);
+
+
+    function ChangeButtons () {
+        setIsActive = (!isActive);
+    };
 
     return (
         <Fragment>
@@ -18,13 +26,19 @@ const Header = props => {
                     </div>
                 </div>
                 <div className={classes.delivery}>
-                    <div className={classes.deliveryChoice}>Delivery</div>
+                    <div
+                        className={isActive ? classes.deliveryAction : classes.deliveryChoice}
+                        onClick={ChangeButtons}
+                    >Delivery</div>
                     <div className={classes.or}>or</div>
-                    <div className={classes.pickUp}>PickUp</div>
+                    <div
+                        className={isActive ? classes.pickUpAction : classes.pickUpChoice}
+                        onClick={ChangeButtons}
+                    >Pick Up</div>
                 </div>
                 <div>
                     <div className={classes.deliveryAdress}>
-                       Delivery to: Gabriel Jönssonsgatan 8
+                        Delivery to: Gabriel Jönssonsgatan 8
                     </div>
                 </div>
 
