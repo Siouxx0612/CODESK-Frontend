e641e8c0dd95dd3d1e4d3fdaafa38522b99c9e4c
Matija <siouxx0612@gmail.com>
2022-05-07 15:21:12 +0200

    update Delivery Choices

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -10,67 +10,57 @@ const reload = () => {
 
 const Header = props => {
 
-const [isDeliveryActive, setIsDeliveryActive] = useState(true);
-const [isPickupAtive, setIsPickupActive] = useState(false);
+    const [isDeliveryActive, setIsDeliveryActive] = useState(true);
+    const [isPickupAtive, setIsPickupActive] = useState(false);
 
-const handleMouseClick = (event) => {
-    if (event.target.className.includes('deliveryOption')) {
-        setIsDeliveryActive(true);
-        setIsPickupActive(false)
-        console.log(isDeliveryActive, isPickupAtive)
-    } else if(event.target.className.includes('pickUpOption')) {
-        setIsDeliveryActive(false);
-        setIsPickupActive(true)
+    const handleMouseClick = (event) => {
+        if (event.target.className.includes('deliveryOption')) {
+            setIsDeliveryActive(true);
+            setIsPickupActive(false)
+            console.log(isDeliveryActive, isPickupAtive)
+        } else if (event.target.className.includes('pickUpOption')) {
+            setIsDeliveryActive(false);
+            setIsPickupActive(true)
+            console.log(isDeliveryActive, isPickupAtive)
+        }
     }
-} 
 
-// function changeButtons(e) {
-//     if (e.target.className.includes(classes.deliveryAction)) {
-//         setIsDeliveryActive(!isDeliveryActive)
-//     } else if (e.target.className.includes(classes.pickUpAction)) {
-//         setIsPickupActive(!isPickupAtive)
-//     }
-// };
+    return (
+        <Fragment>
+            <header className={classes.header}>
+                <div className={classes.logo} onClick={reload}>
+                    <div >
+                        Foodzilla
+                    </div>
+                </div>
+                <div className={classes.delivery}>
+                    <div
+                        className={classes.deliveryOption}
+                        onClick={handleMouseClick}
+                    >Delivery</div>
+                    <div className={classes.or}>or</div>
+                    <div
+                        className={classes.pickUpOption}
+                        onClick={handleMouseClick}
+                    >Pick Up</div>
+                </div>
+                <div>
+                    <div className={classes.deliveryAdress}>
+                       {isDeliveryActive ? <p>Delivery:</p> : <p>PickUp: </p>}
+                    </div>
+                </div>
 
-return (
-    <Fragment>
-        <header className={classes.header}>
-            <div className={classes.logo} onClick={reload}>
-                <div >
-                    Foodzilla
+                <div className={classes.deliveryTime}>
+                    <div >
+                        Choose time: Now
+                    </div>
                 </div>
+                <HeaderCartButton onClick={props.onShowCart} ></HeaderCartButton>
+            </header>
+            <div className={classes['main-image']}>
+                <img src={mealsImg} />
             </div>
-            <div className={classes.delivery}>
-                <div
-                    className={classes.deliveryOption}
-                    onClick={handleMouseClick}
-                    // className={isDeliveryActive ? classes.deliveryAction : classes.deliveryChoice}
-                 
-                >Delivery</div>
-                <div className={classes.or}>or</div>
-                <div
-                    className={classes.pickUpOption}
-                    // className={isPickupAtive ? classes.pickUpAction : classes.pickUpChoice}
-                    
-                >Pick Up</div>
-            </div>
-            <div>
-                <div className={classes.deliveryAdress}>
-                    Delivery to: Gabriel Jönssonsgatan 8
-                </div>
-            </div>
-
-            <div className={classes.deliveryTime}>
-                <div >
-                    Choose time: Now
-                </div>
-            </div>
-            <HeaderCartButton onClick={props.onShowCart} ></HeaderCartButton> 
-        </header>
-        <div className={classes['main-image']}>
-            <img src={mealsImg} />
-        </div>
-    </Fragment>
+        </Fragment>
     )
 };
 
